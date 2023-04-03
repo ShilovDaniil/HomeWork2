34d85b24c1820cbcb2834c0108bd72c098d527bd
DaniilShilov <shilovdanv@gmail.com>
2023-04-03 22:49:32 +0400

    add Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿Main();
+
+void Main()
+{
+  Console.Write("Введите трёхзначное число: ");
+  int number = int.Parse(Console.ReadLine());
+  int secondDigit = (number / 10) % 10;
+  Console.WriteLine($"Вторая цифра числа {number} равна {secondDigit}");
+}
