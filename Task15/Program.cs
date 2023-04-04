f4967059520957b887036b8f7511b86b4e6bcc74
DaniilShilov <shilovdanv@gmail.com>
2023-04-04 14:15:31 +0400

    add Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,20 @@
+﻿Main();
+
+void Main()
+{
+  System.Console.WriteLine("Введите число от 1 до 7: ");
+  int number = int.Parse(System.Console.ReadLine());
+
+  if(number>0 && number<6)
+  {
+    System.Console.WriteLine("No");
+  }
+  else if(number>5 && number<8)
+  {
+    System.Console.WriteLine("Yes");
+  }
+  else if(number<1 && number>7)
+  {
+    System.Console.WriteLine("Error!");
+  }
+}
