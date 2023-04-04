0d5b05ec1c90aca70c619cf06dd8c5b2c6a1d8ec
DaniilShilov <shilovdanv@gmail.com>
2023-04-04 13:56:39 +0400

    add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,29 @@
+﻿Main();
+
+void Main()
+{
+  Console.Write("Введите число: ");
+  int number = int.Parse(Console.ReadLine());
+
+  if(number<100000 && number>10000)
+  {
+    int thirdDigit = (number / 100) % 10;
+    Console.WriteLine($"Третья цифра числа {number} равна {thirdDigit}.");
+  }
+  else if(number>999 && number<10000)
+  {
+    int thirdDigit = (number / 10) % 10;
+    System.Console.WriteLine($"Третья цифра числа {number} равна {thirdDigit}.");
+  }
+
+  else if(number>99 && number<1000)
+  {
+    int thirdDigit = number % 10;
+    System.Console.WriteLine($"Третья цифра числа {number} равна {thirdDigit}.");
+  }
+
+  else if(number<100)
+  {
+    System.Console.WriteLine("Третьей цифры числа нет");
+  }
+}
